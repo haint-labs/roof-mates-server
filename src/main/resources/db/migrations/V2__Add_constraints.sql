ed4ece043edf2b8a42b1d2712a608eff0d8d1ecb
sashjakk <sashjakk@gmail.com>
2022-02-25 23:37:40 +0200

    Add more constraints to users, regions & spots

--- src/main/resources/db/migrations/V2__Add_constraints.sql
+++ src/main/resources/db/migrations/V2__Add_constraints.sql
@@ -1,1 +1,6 @@
 alter table users alter column spot_id drop not null;
+alter table users add unique (phone_number);
+
+alter table regions add unique (address);
+
+alter table spots add unique (parking_number);
